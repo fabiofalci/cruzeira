5d58a5f3a5139593954cd04e4381423fbd3a8bbb
Fabio Falci <fabiofalci@gmail.com>
2013-05-13 23:21:09 +0100

    Enable only compiled files for reloading

--- cruzeira/src/main/java/org/cruzeira/server/ServerManager.java
+++ cruzeira/src/main/java/org/cruzeira/server/ServerManager.java
@@ -33,7 +33,7 @@ public class ServerManager {
 	final Logger logger = LoggerFactory.getLogger(ServerManager.class);
 
 	public ServerManager() {
-		this(new FileSystemChangesNIO("src"), null);
+		this(new FileSystemChangesNIO("src", ".java", ".class", ".xml", ".properties"), null);
 	}
 	
 	public ServerManager(FileSystemChanges fileSystemChanges, Class<? extends WebContext> webContextClass) {
